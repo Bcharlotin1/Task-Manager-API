a5334a10707be6bd5f9e5dd233466bf351f03f8d
bcharlotin1 <biancacharlotin1@gmail.com>
2021-12-03 11:51:10 -0500

    Added new username field to devis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
@@ -9,5 +10,14 @@ class ApplicationController < ActionController::API
 
   def render_not_found_response(exception)
     render json: { error: exception.message }, status: :not_found
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create([
-    {username: 'Sally', password: 'Sally'}
+    {username: 'Sally', password: 'Password'}
 ])
 
 Project.create([
